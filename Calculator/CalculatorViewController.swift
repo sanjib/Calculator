40e088b88c2a49319acd83e4451a41e33d6bf034
Sanjib Ahmad <sanjib@sanjib.org>
2015-10-24 12:48:47 +0600

    feat: Add sin, cos and Pi operations
    Resolves: Required Tasks #3

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -35,6 +35,9 @@ class CalculatorViewController: UIViewController {
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π": displayValue = M_PI; enter()
         default: break
         }
     }
